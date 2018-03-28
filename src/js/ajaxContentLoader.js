3b823b7efc2a244af0c15784d46f536860a87b38
Eugeny Ternovoy <pollitra2008@gamil.com>
2018-03-28 17:46:23 +0700

    fix

--- src/js/ajaxContentLoader.js
+++ src/js/ajaxContentLoader.js
@@ -12,62 +12,113 @@
         }, options);
 
         var methods = {
-            ajaxFilter: function ($form) {
-                var $wrapper = $('#' + $form.data('for')),
-                    $container = $wrapper.find(settings.containerSelector),
-                    $control = $wrapper.find(settings.controlSelector),
-                    $pager = $wrapper.find(settings.pagerSelector);
-
+            filterRequest      : function ($form) {
                 $.ajax({
                     url     : $form.attr('action'),
                     data    : $form.serialize(),
                     dataType: 'JSON',
                     method  : 'GET',
-                    success : function (data) {
-                        if (data !== undefined) {
-
-                            if (data.loop !== undefined && data.loop !== null ) {
-                                $container.html(data.loop);
-                            }
-
-                            if (data.moreButtons !== undefined && data.moreButtons !== null) {
-                                if ($control.length > 0) {
-                                    if (data.moreButtons.length > 10) {
-                                        $control.html(data.moreButtons);
-                                    } else {
-                                        $control.html('');
-                                    }
-                                } else {
-                                    var btnsForModification = $wrapper.find('.js--ajaxBtn').filter(function () {
-                                        return $(this).closest('.js--ajaxPager').length === 0;
-                                    });
-
-                                    btnsForModification.each(function () {
-                                        if(data.moreButtons.length < 10) {
-                                            $(this).addClass('is--hide');
-                                        } else {
-                                            $(this).replaceWith(data.moreButtons);
-                                        }
-                                    });
-                                }
-                            }
-
-                            if (typeof data.pager === 'string') {
-                                if (data.pager.length < 10) {
-                                    $pager.html('');
-                                } else {
-                                    $pager.replaceWith(data.pager);
-                                }
-                            }
-                        }
-                    },
-                    error   : function () {
-
-                    },
+                    context : $form[0],
+                    success : methods._onSuccess,
                     complete: function () {
-                        window.history.replaceState(null, null, this.url);
+                        var $context = $(this);
+                        if($context.is('form')) {
+                            window.history.replaceState(null, null, $context.attr('action') + '?' + $context.serialize());
+                        }
                     }
                 });
+            },
+            btnRequest : function ($btn) {
+                $.ajax({
+                    url       : $btn.attr('href'),
+                    dataType  : 'JSON',
+                    method    : 'POST',
+                    cache     : false,
+                    context   : this,
+                    beforeSend: function () {
+                        $btn.addClass('loading');
+                    },
+                    success   : methods._onSuccess,
+                    complete  : function () {
+                        $btn.removeClass('loading');
+                    }
+                });
+            },
+            _onSuccess         : function (data) {
+                var $context = $(this),
+                    $wrapper = $('#' + $context.data('for')),
+                    $container = $wrapper.find(settings.containerSelector),
+                    isFilter = $context.is('form');
+
+                if (methods._isExist(data)) {
+                    var isPagerBtn = methods._isPagerButton($context),
+                        url = $context.attr('href');
+
+                    if (methods._isExist(data.loop)) {
+                        if (isPagerBtn || isFilter) {
+                            methods._replaceLoop($container, data.loop)
+                        } else {
+                            methods._appendLoop($container, data.loop)
+                        }
+                    }
+
+                    if (methods._isExist(data.moreButtons)) {
+                        if (methods._isExist(url)) {
+                            window.history.replaceState(null, null, url);
+                        }
+
+                        methods._replaceMoreButtons($wrapper, $context, data.moreButtons);
+                    }
+
+                    if (methods._isExist(data.pager)) {
+                        methods._replacePager($wrapper, data.pager)
+                    }
+                }
+            },
+            _replaceLoop       : function ($container, loop) {
+                $container.html(loop)
+            },
+            _appendLoop        : function ($container, loop) {
+                $container.append(loop);
+            },
+            _isPagerButton     : function ($button) {
+                return $button.closest(settings.pagerSelector).length > 0;
+            },
+            _isExist           : function (element) {
+                return element !== undefined && element !== null
+            },
+            _replaceMoreButtons: function ($wrapper, $context, dataBtn) {
+                var $control = $wrapper.find(settings.controlSelector),
+                    dataIsEmpty = dataBtn.indexOf('<') === -1;
+
+                if ($control.length > 0) {
+                    if (dataBtn.indexOf('<') > -1) {
+                        $control.html(dataBtn);
+                    } else {
+                        $control.html('');
+                    }
+                } else {
+                    var btnsForModification = $(settings.btnSelector + '[data-for="' + $context.data('for') + '"]').filter(function () {
+                        return $(this).closest(settings.pagerSelector).length === 0;
+                    });
+
+                    btnsForModification.each(function () {
+                        if (dataIsEmpty) {
+                            $(this).addClass('is--hide');
+                        } else {
+                            $(this).replaceWith(dataBtn);
+                        }
+                    });
+                }
+            },
+            _replacePager      : function ($wrapper, dataPager) {
+                var $pager = $wrapper.find(settings.pagerSelector);
+
+                if (dataPager.indexOf('<') === -1) {
+                    $pager.html('');
+                } else {
+                    $pager.html(dataPager);
+                }
             }
         };
 
@@ -77,80 +128,16 @@
             e.preventDefault();
             e.stopPropagation();
 
-            var $btn = $(this),
-                $wrapper = $('#' + $btn.data('for')),
-                $pager = $wrapper.find(settings.pagerSelector),
-                $container = $wrapper.find(settings.containerSelector),
-                $control = $wrapper.find(settings.controlSelector),
-                url = $btn.attr('href');
-
-            var isPagerBtn = $btn.closest(settings.pagerSelector).length > 0;
-
-            $.ajax({
-                url       : url,
-                dataType  : 'JSON',
-                method    : 'POST',
-                cache     : false,
-                beforeSend: function () {
-                    $btn.addClass('loading');
-                },
-                success   : function (data) {
-                    if (data.loop !== undefined && data.loop !== null) {
-                        if (isPagerBtn) {
-                            $container.html(data.loop);
-                        } else {
-                            $container.append(data.loop);
-                        }
-                    }
-
-                    if (data.moreButtons !== undefined && data.moreButtons !== null) {
-                        window.history.replaceState(null, null, url);
-
-                        if (isPagerBtn) {
-                            var btnsForModification = $(settings.btnSelector + '[data-for="' + $btn.data('for') + '"]').filter(function () {
-                                return $(this).closest(settings.pagerSelector).length === 0;
-                            });
-
-                            btnsForModification.each(function () {
-                                replaceMoreButton($(this));
-                            })
-                        } else {
-                            replaceMoreButton($btn);
-                        }
-                    }
-
-                    if (typeof data.pager === 'string') {
-                        $pager.replaceWith(data.pager);
-                    }
-
-                    function replaceMoreButton($button) {
-                        if ($control.length > 0) {
-                            if (data.moreButtons.length > 10) {
-                                $control.html(data.moreButtons);
-                            } else {
-                                $control.html('');
-                            }
-                        }
-                        else {
-                            if (data.moreButtons.length > 10) {
-                                $button.replaceWith(data.moreButtons);
-                            } else {
-                                $button.addClass('is--hide');
-                            }
-                        }
-                    }
-
-                },
-                complete  : function () {
-                    $btn.removeClass('loading');
-                }
-            });
+            methods.btnRequest($(this))
         });
 
         $body.on('change', settings.filterSelector, function (e) {
-            methods.ajaxFilter($(e.target).closest('form'));
+            methods.filterRequest($(e.target).closest('form'));
         });
-
     }
 
-})(JQuery);
+})(jQuery);
+
+$(document).ready(function () {
+    $.fn.ajaxContentLoader();
+});
